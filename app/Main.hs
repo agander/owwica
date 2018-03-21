9a0b3a2c9b3d47e4522f9ec5c354886c65f6f210
agander <agander@gmailcom>
2018-03-21 15:55:34 +0100

    [0.0.3]: func: str_to_filepath done; [Char] paths now converted to [FilePath].

--- app/Main.hs
+++ app/Main.hs
@@ -9,7 +9,24 @@ main = do
   putStrLn "hello world"
 
   -- | Get a String from getEnv "PATH"
+  -- | return String
   path <- getEnv "PATH"
 
+  -- | Split the path by ":"
+  -- | return [[Char]]
+  let paths = splitOn ":" path
+
+  -- | Convert the [[Char]] to [FilePath]
+  let fp_paths = map str_to_filepath paths
+
+  -- | foreach path ([Char]): 
+
   putStrLn "ciao"
+
+-- | Convert paths into [FilePath]
+str_to_filepath :: [Char] -> FilePath
+str_to_filepath [] = []
+str_to_filepath (x:xs) = x : str_to_filepath xs
+
+-- | lists of lists to list
 
--- m4/version.m4
+++ m4/version.m4
@@ -1,1 +1,1 @@
-define([VERSION_NUMBER],[0.0.2])dnl
+define([VERSION_NUMBER],[0.0.3])dnl
