be9fd0160a8bfee8b97f1dc3f95688eb55067ed9
agander <1449985+agander@users.noreply.github.com>
2018-03-21 22:27:29 +0100

    Forgot to add Lib.hs

---
+++ src/Lib.hs
@@ -0,0 +1,103 @@
+module Lib
+    ( getAbsoluteDirContents
+    , str_to_text
+    , text_to_str
+    , get_paths
+    , getAllPathContents
+    --, myzip
+    --, myzipWith     
+    ) where
+
+import System.Directory (getDirectoryContents)
+import System.FilePath ((</>))
+import qualified Data.Text as T
+import Data.List.Split (splitOn)
+import qualified Data.List as L
+import Data.Foldable (foldMap)
+import qualified Data.ByteString as B
+
+-- | No idea what this does
+getAllPathContents :: Monad m => [FilePath] -> m [IO [FilePath]]
+getAllPathContents paths = do
+  --allFiles <- map getAbsoluteDirContents paths
+  let allFiles = map getAbsoluteDirContents paths
+  --return $ map T.pack allFiles
+  return allFiles
+
+{-
+ - © Paolo Capriotti
+ - <https://stackoverflow.com/questions/8572196/directory-contents-in-haskell/8572265#8572265>
+ - I added the T.pack output
+getAbsoluteDirContents :: String -> IO [FilePath]
+-}
+getAbsoluteDirContents :: FilePath -> IO [FilePath]
+getAbsoluteDirContents dir = do
+  contents <- getDirectoryContents dir
+  --let fullFilePaths = map (dir </>) contents
+  return $ map (dir </>) contents
+
+str_to_text :: String -> T.Text
+  --path <- T.pack $ getEnv "PATH"
+str_to_text = T.pack
+
+text_to_str :: T.Text -> String
+  --path <- T.pack $ getEnv "PATH"
+text_to_str = T.unpack
+
+-- From a String (${PATH}), return a List-of-Lists split on ':'
+get_paths :: Eq a => [a] -> [a] -> [[a]]
+get_paths path delimiter = splitOn delimiter path
+
+toText :: [FilePath] -> T.Text
+toText = foldMap T.pack
+
+datumT :: T.Text
+datumT = T.pack "cassava_eg.sh~\n\
+  \Setup.hs\n\
+  \.SimpleFFI.hs.un~\n\
+  \stack.yaml\n.\n\
+  \LTSB_sett_2017.csv\n\
+  \dtmpl.cabal~\n\
+  \LICENSE\ncassava_eg.sh\n\
+  \dtmpl.cabal.un~\n\
+  \git\n\
+  \items.csv\n\
+  \cassava_eg.txt\n\
+  \gash.un~\n\
+  \fsconfig.hs.un~\n\
+  \SimpleFFI.hs~\n\
+  \m4\n\
+  \app\n\
+  \fsconfig.sh~\n\
+  \storia_it.html\n\
+  \dtmpl.cabal\n\
+  \SimpleFFI.hs\n\
+  \stack.yaml~\n\
+  \README.md\n\
+  \src\n\
+  \bytestr1.sh~\n\
+  \test\n\
+  \stack.yaml.un~\n\
+  \fsconfig.sh\n\
+  \fsconfig.hs~\n\
+  \bytestr1.sh\n\
+  \.gitignore\n\
+  \.stack-work\n\
+  \gash\n\
+  \dsdt.dat\n\
+  \.fsconfig.sh.un~\n"
+
+
+myzip :: [a] -> [b] -> [(a, b)]
+myzip [] [] = []
+myzip [_] [] = []
+myzip [] [_] = []
+myzip (x:xs) (y:ys) = (x,y) : myzip xs ys
+
+myzipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myzipWith f [] [] = []
+myzipWith f [_] [] = []
+myzipWith f [] [_] = []
+myzipWith f (x:xs) (y:ys) = x `f` y : myzipWith f xs ys
+
+
